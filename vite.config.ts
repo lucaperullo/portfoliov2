61bde46f5fe1c96e917f8defffa31d4e5c34873d
lucaperullo <lucaperullo@outlook.it>
2024-09-21 14:31:03 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/portfoliov2", // Replace 'your-repo-name' with the actual repository name on GitHub
+});
